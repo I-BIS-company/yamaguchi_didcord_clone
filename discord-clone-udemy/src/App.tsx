07d2d257ab98e3fe0830ee38add8a91b64ab1ef7
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-06-10 19:41:26 +0900

    discord-clone-udemyフォルダーのコンテンツをrootに移動 自作のgitignoreを削除

--- discord-clone-udemy/src/App.tsx
+++ discord-clone-udemy/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
-  return  <div className="App"></div>
+  return  (
+  <div className="App">
+    {/* sidebar */}
+    <Sidebar />
+    {/* home */}
+
+  </div>
+  
+  );
 }
 
 export default App;
