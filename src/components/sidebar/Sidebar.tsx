48b4b60fdd0d025a98cc9db6e3a1ad9f9997675f
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-06-11 23:52:47 +0900

    Discordチャンネルリストのヘッダー・リストを作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Sidebar.scss"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
 
 const Sidebar = () => {
   return (
@@ -20,6 +22,24 @@ const Sidebar = () => {
                 <h3>Discord</h3>
                 <ExpandMoreIcon />
 
+            </div>
+
+            {/* sidebarChannels */}
+            <div className="sidebarChannels">
+                <div className="sidebarChannelsHeader">
+                    <div className="sidebarHeader">
+                    <ExpandMoreIcon />
+                    <h4>プログラミングチャンネル</h4>
+                    </div>
+                    <AddIcon className='sidebarAddIcon' />
+                </div>
+
+                <div className="sidebarChannelList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>
             </div>
         </div>
     </div>
