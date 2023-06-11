921a89c0ddd5177a20791ecfe0825b110dfab7a2
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-06-12 00:33:48 +0900

    chatコンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/chat/Chat';
 
 function App() {
   return  (
   <div className="App">
+    {/* sidebar */}
     <Sidebar />
+
+    {/* chat */}
+    <Chat />
   </div>
+
+    
   
   );
 }
